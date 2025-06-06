f3ac1025a4c6373bd6732fe456f93ab689f0d997
Kuan2025 <kuan.arol.5a211@gmail.com>
2025-06-07 00:10:38 +0300

    Add files via upload

---
+++ js script.js
@@ -0,0 +1,16 @@
+var username = "kuan";
+var age = 25;
+var isAdmin = true;
+console.log(username);
+
+// if (username == 'kuan && age == 25 && isAdmin == true') {
+//     console.log("hello");
+// } else {
+//     console.log("Etlas3 bara");
+// }
+var i = 5;
+while (i < 5) {
+    console.log(i);
+    i++;
+}
+
